17d103811bab5109d02a07f3970a18f3a630b9a3
Esca-MMC <esca.mmc@gmail.com>
2024-12-29 23:52:28 -0600

    Rewrite add_bush command, add townBush and offset args

--- DestroyableBushes/Console Commands/AddBush.cs
+++ DestroyableBushes/Console Commands/AddBush.cs
@@ -7,137 +7,144 @@ namespace DestroyableBushes
 {
     public static partial class Commands
     {
-        /// <summary>SMAPI console command. Creates a new bush with the specified size.</summary>
+        /// <summary>SMAPI console command. Creates a new bush instance with specific properties.</summary>
         /// <param name="command">The console command's name.</param>
         /// <param name="args">The space-delimited arguments entered with the command.</param>
+        /// <remarks>Expected format: add_bush &lt;int size&gt; [bool townBush] [int tileSheetOffset] [int x int y] [string location]</remarks>
         public static void AddBush(string command, string[] args)
         {
             if (!Context.IsWorldReady) //if the player is not currently in a loaded game
-                return; //do nothing
+                return; //do nothing                
+
+            /* parse args */
 
             if (args.Length <= 0) //"add_bush" (invalid)
             {
-                ModEntry.Instance.Monitor.Log($"Invalid number of arguments. Please include a bush size, e.g. \"{command} {Bush.largeBush}\". Type \"help {command}\" for formatting information.", LogLevel.Info);
+                ModEntry.Instance.Monitor.Log($"Invalid number of arguments. Please include a bush size, e.g. \"{command} {Bush.largeBush}\". Type \"help {command}\" for more info.", LogLevel.Info);
                 return;
             }
-            else if (args.Length == 2) //"add_bush <size> [x]" (invalid)
+            else if (args.Length == 4) //"add_bush <size> [townBush] [tileSheetOffset] [x]" (invalid)
             {
-                ModEntry.Instance.Monitor.Log($"Invalid number of arguments. Please include both X and Y, e.g. \"{command} {Bush.largeBush} 64 19\". Type \"help {command}\" for formatting information.", LogLevel.Info);
-                return;
-            }
-
-            int? size = ParseBushSize(args[0]); //attempt to parse the size argument
-
-            if (size == null) //if parsing failed
-            {
-                ModEntry.Instance.Monitor.Log($"\"{args[0]}\" is not a recognized bush size. Type \"help {command}\" for formatting information.", LogLevel.Info);
+                ModEntry.Instance.Monitor.Log($"Invalid number of arguments. Please include both X and Y, e.g. \"{command} {Bush.largeBush} false 0 64 19\". Type \"help {command}\" for more info.", LogLevel.Info);
                 return;
             }
 
-            int x;
-            int y;
-
-            if (args.Length >= 3) //if x y arguments were provided
+            if (!TryParseBushSize(args[0], out int size)) //"add_bush <size>"
             {
-                if (int.TryParse(args[1], out x) == false) //try to parse the x argument; if it fails,
-                {
-                    ModEntry.Instance.Monitor.Log($"\"{args[1]}\" could not be parsed as an integer (x). Type \"help {command}\" for formatting information.", LogLevel.Info);
-                    return;
-                }
-                else if (int.TryParse(args[2], out y) == false) //try to parse the y argument; if it fails,
-                {
-                    ModEntry.Instance.Monitor.Log($"\"{args[2]}\" could not be parsed as an integer (y). Type \"help {command}\" for formatting information.", LogLevel.Info);
-                    return;
-                }
+                ModEntry.Instance.Monitor.Log($"\"{args[0]}\" is not a recognized size value. Type \"help {command}\" for formatting information.", LogLevel.Info);
+                return;
             }
-            else //if x y arguments were NOT provided
+
+            bool townBush = false;
+            if (args.Length > 1 && !ArgUtility.TryGetBool(args, 1, out townBush, out _, "bool townBush")) //"add_bush <size> [townBush]"
             {
-                //get the player's current tile position
-                Vector2 tile = Game1.player.Tile;
-                x = (int)tile.X;
-                y = (int)tile.Y;
+                ModEntry.Instance.Monitor.Log($"\"{args[1]}\" is not a recognized townBush value. It should be \"true\" or \"false\". Type \"help {command}\" for more info.", LogLevel.Info);
+                return;
+            }
+
+            int tileSheetOffset = 0;
+            if (args.Length > 2 && !ArgUtility.TryGetInt(args, 2, out tileSheetOffset, out _, "int tileSheetOffset")) //"add_bush <size> [townBush] [tileSheetOffset]"
+            {
+                ModEntry.Instance.Monitor.Log($"\"{args[2]}\" is not a recognized tileSheetOffset value. It should be an integer, e.g. \"0\". Type \"help {command}\" for more info.", LogLevel.Info);
+                return;
+            }
+
+            Vector2 tile;
+            bool usingPlayerTile;
+
+            if (args.Length <= 4) //if x and y weren't provided as args
+            {
+                tile = Game1.player.Tile;
+                usingPlayerTile = true;
+            }
+            else if (!ArgUtility.TryGetVector2(args, 3, out tile, out _, true, "Vector2 tile")) //"add_bush <size> [townBush] [tileSheetOffset] [x y]"
+            {
+                ModEntry.Instance.Monitor.Log($"\"{args[3]} {args[4]}\" are not recognized x and y values. They should be integers, e.g. \"64 19\". Type \"help {command}\" for more info.", LogLevel.Info);
+                return;
+            }
+            else //if x and y were provided as args, and parsing succeeded
+            {
+                usingPlayerTile = false;
             }
 
             GameLocation location;
 
-            if (args.Length >= 4) //if a location argument was provided
+            if (args.Length > 5 && args[5] != null) //"add_bush <size> [townBush] [tileSheetOffset] [x y] [location]"
             {
-                //combine the remaining arguments into a single string
-                string locationName = args[3]; //get the first location argument
-                for (int count = 4; count < args.Length; count++) //for each argument after args[3]
-                {
-                    locationName += " " + args[count]; //add this argument to the location name, separated by a space
-                }
-
-                location = Game1.getLocationFromName(locationName); //attempt to get a location with the combined name
+                location = Game1.getLocationFromName(args[5]); //attempt to get a location with the combined name
                 if (location == null) //if no location matched the name
                 {
-                    ModEntry.Instance.Monitor.Log($"No location named \"{locationName}\" could be found. Type \"help {command}\" for formatting information.", LogLevel.Info);
+                    ModEntry.Instance.Monitor.Log($"No location named \"{args[5]}\" could be found. Type \"help {command}\" for more info.", LogLevel.Info);
                     return;
                 }
             }
-            else
+            else //location wasn't provided as an argument
             {
                 location = Game1.player.currentLocation; //use the player's current location
-                if (location == null) //if the player's location was null
-                    return; //do nothing
+                if (location == null) //if the player doesn't have a location for some reason
+                    return;
             }
 
-            //all necessary information has been parsed
+            /* args parsed; spawn bush */
 
-            Bush bush = new Bush(new Vector2(x, y), size.Value, location); //create the new bush
+            Bush bush = new Bush(tile, size, location);
 
-            if (size.Value == Bush.walnutBush)
+            bush.townBush.Value = townBush;
+            bush.tileSheetOffset.Value = tileSheetOffset;
+            bush.setUpSourceRect(); //update the bush's sprite (necessary if the fields above change)
+
+            if (usingPlayerTile) //if the player's tile was used to spawn this bush
             {
-                bush.tileSheetOffset.Value = 0; //remove walnut (note: rewrite would be needed to support spawning both ways, and if players want free walnuts, they can use debug)
-                bush.setUpSourceRect(); //update sprite
-            }
-
-            Rectangle playerBox = Game1.player.GetBoundingBox();
-
-            while (playerBox.Intersects(bush.getBoundingBox())) //while this bush is colliding with the player
-            {
-                switch (Game1.player.FacingDirection) //based on which direction the player is facing
+                Rectangle playerBox = Game1.player.GetBoundingBox();
+                while (playerBox.Intersects(bush.getBoundingBox())) //while this bush is colliding with the player
                 {
-                    default: //unknown facing position (should be unreachable)
-                    case 0: //up
-                        bush.Tile = new Vector2(bush.Tile.X, bush.Tile.Y - 1); //move bush up 1 tile
-                        break;
-                    case 1: //right
-                        bush.Tile = new Vector2(bush.Tile.X + 1, bush.Tile.Y); //move bush right 1 tile
-                        break;
-                    case 2: //down
-                        bush.Tile = new Vector2(bush.Tile.X, bush.Tile.Y + 1); //move bush down 1 tile
-                        break;
-                    case 3: //left
-                        bush.Tile = new Vector2(bush.Tile.X - 1, bush.Tile.Y); //move bush left 1 tile
-                        break;
+                    switch (Game1.player.FacingDirection) //based on which direction the player is facing
+                    {
+                        default: //unknown facing position (should be unreachable)
+                        case 0: //up
+                            bush.Tile = new Vector2(bush.Tile.X, bush.Tile.Y - 1); //move bush up 1 tile
+                            break;
+                        case 1: //right
+                            bush.Tile = new Vector2(bush.Tile.X + 1, bush.Tile.Y); //move bush right 1 tile
+                            break;
+                        case 2: //down
+                            bush.Tile = new Vector2(bush.Tile.X, bush.Tile.Y + 1); //move bush down 1 tile
+                            break;
+                        case 3: //left
+                            bush.Tile = new Vector2(bush.Tile.X - 1, bush.Tile.Y); //move bush left 1 tile
+                            break;
+                    }
                 }
             }
 
             location.largeTerrainFeatures.Add(bush); //add the bush to the location
         }
 
-        /// <summary>Parses the "size" argument of a console command. Returns null if parsing fails.</summary>
-        /// <param name="size">The bush size argument.</param>
-        /// <returns>The integer representing this bush's size. Null if parsing fails.</returns>
-        private static int? ParseBushSize(string size)
+        /// <summary>Attempts to parse a text string describing a bush's size (a.k.a. type) into an integer value.</summary>
+        /// <param name="rawSize">A string describing a bush size.</param>
+        /// <param name="parsedSize">The parsed integer value for this bush size. 0 if parsing failed.</param>
+        /// <returns>True if parsing succeeded. False if parsing failed, i.e. if the raw size was not an integer or recognized name.</returns>
+        private static bool TryParseBushSize(string rawSize, out int parsedSize)
         {
-            switch (size.ToLower()) //based on the size string (converted to lowercase)
+            string normalizedRawSize = rawSize?.Trim().ToLower();
+            switch (normalizedRawSize)
             {
                 case "0":
                 case "s":
                 case "small":
-                    return Bush.smallBush;
+                    parsedSize = Bush.smallBush;
+                    return true;
                 case "1":
                 case "m":
                 case "med":
                 case "medium":
-                    return Bush.mediumBush;
+                    parsedSize = Bush.mediumBush;
+                    return true;
                 case "2":
                 case "l":
                 case "large":
-                    return Bush.largeBush;
+                    parsedSize = Bush.largeBush;
+                    return true;
                 case "3":
                 case "t":
                 case "green":
@@ -145,13 +152,16 @@ namespace DestroyableBushes
                 case "greenteabush":
                 case "tea":
                 case "teabush":
-                    return Bush.greenTeaBush;
+                    parsedSize = Bush.greenTeaBush;
+                    return true;
                 case "4":
                 case "w":
                 case "walnut":
-                    return Bush.walnutBush;
+                    parsedSize = Bush.walnutBush;
+                    return true;
                 default:
-                    return null;
+                    return int.TryParse(normalizedRawSize, out parsedSize); //true if the input can be parsed into any other integer
+
             }
         }
     }
--- DestroyableBushes/ModEntry.cs
+++ DestroyableBushes/ModEntry.cs
@@ -32,16 +32,20 @@ namespace DestroyableBushes
                 documentation: "Creates a bush of the specified size.\n" +
                                "Bushes added by this command will regrow if that setting is enabled.\n" +
                                "\n" +
-                               "Usage: add_bush <size> [x y] [location]\n" +
+                               "Usage: add_bush <size> [bool townBush] [int tileSheetOffset] [int x int y] [string location]\n" +
                                "- size: The bush's size, as a name or number. 0 = \"small\", 1 = \"medium\", 2 = \"large\", 3 = \"tea\", 4 = \"walnut\".\n" +
+                               "- townBush (optional): Whether this bush should use a \"town bush\" sprite. This should be \"true\" or \"false\". False if not provided. Some bush sizes don't have town sprites.\n" +
+                               "- tileSheetOffset (optional): The \"tilesheet offset\" for this bush, as a number. 0 if not provided. If set to 1, medium bushes will have berries, and walnut bushes will have walnuts.\n" +
                                "- x y (optional): The bush's tile coordinates. If not provided, the bush will appear in front of the player.\n" +
-                               "- location (optional): The name of the bush's map, e.g. \"Farm\". If not provided, the player's current map will be used.\n" +
+                               "- location (optional): The name of the bush's map, e.g. \"Farm\" or \"BusStop\". If not provided, the player's current map will be used.\n" +
                                "\n" +
                                "Examples:\n" +
                                "  add_bush 2\n" +
                                "  add_bush large\n" +
-                               "  add_bush 2 64 19\n" +
-                               "  add_bush 2 64 19 farm",
+                               "  add_bush 2 false\n" +
+                               "  add_bush 2 false 0\n" +
+                               "  add_bush 2 false 0 64 19\n" +
+                               "  add_bush 2 false 0 64 19 farm",
                 callback: Commands.AddBush
             );
 
